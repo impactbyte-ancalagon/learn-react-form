fd2714d804bd88440b12f58c5993fde1eff6229b
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-09 14:55:49 +0700

    Implement handleTodoInput in TodoApp

--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react"
 
 class TodoApp extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      input: "",
+      todos: [
+        {
+          id: 0,
+          text: "Eat lunch"
+        },
+        {
+          id: 1,
+          text: "Coding for fun"
+        }
+      ]
+    }
+  }
+
+  handleTodoInput = event => {
+    this.setState({ input: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <h1>Todo App</h1>
         <form>
-          <input type="text" placeholder="What do you want to do?" />
+          <input
+            type="text"
+            placeholder="What do you want to do?"
+            onChange={this.handleTodoInput}
+          />
           <input type="submit" value="Add Todo" />
         </form>
         <ul>
-          <li>Eat lunch</li>
-          <li>Coding for fun</li>
+          {this.state.todos.map(todo => {
+            return <li key={todo.id}>{todo.text}</li>
+          })}
         </ul>
       </div>
     )
