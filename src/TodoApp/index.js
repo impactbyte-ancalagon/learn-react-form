ad35d362fd6cd0db1275857f19a1195e5e30fe8d
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-09 15:08:27 +0700

    Implement handleTodoSubmit

--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -23,21 +23,42 @@ class TodoApp extends Component {
     this.setState({ input: event.target.value })
   }
 
+  handleTodoSubmit = event => {
+    event.preventDefault()
+    console.log("TODO SUBMITTED!", this.state.input)
+
+    const newTodo = {
+      id: this.state.todos.length,
+      text: this.state.input
+    }
+    const newTodos = this.state.todos.concat(newTodo)
+
+    this.setState({
+      input: "",
+      todos: newTodos
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Todo App</h1>
-        <form>
+        <form onSubmit={this.handleTodoSubmit}>
           <input
             type="text"
             placeholder="What do you want to do?"
             onChange={this.handleTodoInput}
+            value={this.state.input}
           />
           <input type="submit" value="Add Todo" />
         </form>
         <ul>
-          {this.state.todos.map(todo => {
-            return <li key={todo.id}>{todo.text}</li>
+          {this.state.todos.map((todo, index) => {
+            return (
+              <li key={index}>
+                {todo.id}: {todo.text}
+              </li>
+            )
           })}
         </ul>
       </div>
